b3d431a63d8083d23db327a392335924d993a6a6
Benjamin <benjamindcox02@gmail.com>
2024-04-21 08:39:44 +1000

    jghgfv

--- evaluation_functions.py
+++ evaluation_functions.py
@@ -24,7 +24,7 @@ import deep_learning_helperFuncs
 logging.getLogger('matplotlib.font_manager').disabled = True
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-def eval_model(model, train, Y_train, test, Y_test, model_type):
+def create_confusion_matrix_comparison(model, train, Y_train, test, Y_test, model_type):
     """_summary_
 
     Args:
@@ -68,3 +68,24 @@ def eval_model(model, train, Y_train, test, Y_test, model_type):
 
     # Save the figure
     plt.savefig(os.path.join('plots', model_type + '_ConfMatrix.png'))
+    
+def plot_images(x, y):
+    fig = plt.figure()
+    for i in range(6):
+        ax = fig.add_subplot(3, 2, i + 1)
+        
+        # Check if data is float type and in range [0, 1], or rescale if it's [0, 255]
+        if x[i].dtype == np.float32:
+            img_to_plot = x[i] if x[i].min() >= 0 and x[i].max() <= 1 else x[i] / 255.0
+        else:
+            img_to_plot = x[i] / 255.0
+        
+        ax.imshow(img_to_plot, cmap='gray')  # Use cmap='gray' for grayscale images
+        ax.set_title(y[i])
+        ax.axis('off')
+        # Create the folder if it doesn't already exist
+    if not os.path.exists('plots'):
+        os.makedirs('plots')
+
+    # Save the figure
+    plt.savefig(os.path.join('plots', 'Images.png'))
